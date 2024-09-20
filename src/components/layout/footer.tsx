76cb3f88180254f3c23525c5d67732f6abf557d4
Nhi <nhipham1195@gmail.com>
2024-09-20 12:25:14 +0200

    refactor: error in display

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -15,19 +15,19 @@ export function Footer() {
       <footer className="pt-12 border-t divide-y divide-orange-500 bg-white  dark:bg-primary-800 py-6 ">
         <div className="mx-auto md:flex md:justify-between  md:max-w-7xl  gap-12 p-6 ">
           <div className="md:row-span-7 flex gap-12 mb-9 md:mb-0 md:w-80 justify-center md:justify-between text-primary-700 dark:text-primary-100 font-kennerly text-lg">
-            <Link href={"/"}>Home</Link>
+            <Link href={"/"}>Accueil</Link>
             <Link href={"/#portfolio"}>Projets</Link>
             <Link href={"/blogs"}>Gallerie Photo</Link>
           </div>
 
           <div className="row-span-1 text-primary-500 font-kennerly text-sm md:text-base text-center dark:text-primary-300">
             <p>© 2024 Nhi PHAM. All rights reserved.</p>
-            <p className="max-w-96">
+            <p className="max-w-96 mx-auto">
               All the photos, images, logos and drawings in website are
               designed, created by Nhi PHAM.
             </p>
-            <p className="max-w-96 text-xs italic">
-              Except logos and images concerned Mobalib, LIFAT and CEFIM.
+            <p className="max-w-96 text-xs italic mx-auto">
+              Except logos and images related to Mobalib, LIFAT and CEFIM.
             </p>
           </div>
         </div>
